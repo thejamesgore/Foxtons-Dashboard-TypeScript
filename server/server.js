cccc20af803167741faf6bd15479f0440d09c4e9
James Gore <83005220+thejamesgore@users.noreply.github.com>
2023-03-01 09:22:33 +0000

    init: Basic express server

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json({ limit: '50mb ' }))
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Express server running!' })
+})
+
+const startServer = async () => {
+  try {
+    app.listen(8080, () => {
+      console.log('[SERVER]: Server started http://localhost:8008')
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+startServer()
