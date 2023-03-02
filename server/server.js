70b2519ea0e77478e761cd4f8d54c486dac18160
James Gore <83005220+thejamesgore@users.noreply.github.com>
2023-03-02 16:29:49 +0000

    feat: Add property and user controllers and routes, update server.js

--- server/server.js
+++ server/server.js
@@ -1,23 +1,31 @@
 import express from 'express'
-import dotenv from 'dotenv'
+import * as dotenv from 'dotenv'
 import cors from 'cors'
+
+import connectDB from './mongodb/connect.js'
+import userRouter from './routes/user.routes.js'
+import propertyRouter from './routes/property.routes.js'
 
 dotenv.config()
 
 const app = express()
-
 app.use(cors())
-app.use(express.json({ limit: '50mb ' }))
+app.use(express.json({ limit: '50mb' }))
 
 app.get('/', (req, res) => {
   res.send({ message: 'Express server running!' })
 })
 
+app.use('/api/v1/users', userRouter)
+app.use('/api/v1/properties', propertyRouter)
+
 const startServer = async () => {
   try {
-    app.listen(8080, () => {
+    connectDB(process.env.MONGODB_URL)
+
+    app.listen(8080, () =>
       console.log('[SERVER]: Server started http://localhost:8080')
-    })
+    )
   } catch (error) {
     console.log(error)
   }
